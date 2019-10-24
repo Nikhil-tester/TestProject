510fdd6a3d760f0838c888c9254a3d82ae3c0ecc
nikhil.g <nikhil.g@isslt472.inubesolutions.net>
2019-10-24 12:32:59 +0530

    Calculator

---
+++ Java/src/practice/Trail1_Calculator.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Trail1_Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.println("Enter First number :");
+		int x = scan.nextInt();
+		
+		System.out.println("Enter Second number :");
+		int y = scan.nextInt();
+		
+		int answer = x+y;
+		
+		System.out.println("Answer is : " + answer );
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
